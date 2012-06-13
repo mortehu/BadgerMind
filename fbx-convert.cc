805179a2a542079e53c331743db38130565f71de
Morten Hustveit <morten.hustveit@gmail.com>
2012-06-13 21:01:37 +0200

    Initial commit

---
+++ fbx-convert.cc
@@ -0,0 +1,667 @@
+#include <map>
+#include <vector>
+
+#include <err.h>
+#include <math.h>
+
+#include <fbxsdk.h>
+#include <fbxsdk/utils/fbxutilities.h>
+
+/* KFbxTakeInfo* lCurrentTakeInfo = scene->GetTakeInfo(*(takeNames[pItem])); */
+/* scene->SetCurrentTake(takeNames[pItem]->Buffer()); */
+
+static void
+PreparePointCacheData(KFbxScene* pScene);
+
+static void
+ExportMeshScene(KFbxScene* pScene);
+
+static void
+ExportAnimScene(KFbxScene* pScene, KTime& time);
+
+static KFbxXMatrix
+GetGeometry(KFbxNode* node);
+
+static void
+ConvertNurbsAndPatchRecursive(KFbxSdkManager* pSdkManager,
+                              KFbxNode* node);
+
+struct vertex
+{
+  float x, y, z;
+  float u, v;
+
+  bool
+  operator<(const struct vertex &rhs) const
+    {
+#define TEST(a) if (a != rhs.a) return a < rhs.a;
+
+      TEST(x)
+      TEST(y)
+      TEST(z)
+      TEST(u)
+
+#undef TEST
+
+      return v < rhs.v;
+    }
+};
+
+int
+main(int argc, char** argv)
+{
+  int i;
+  int lFileFormat = -1;
+  KString *gFileName = new KString;
+
+  KFbxSdkManager* fbxSDKManager;
+  KFbxImporter* fbxImporter;
+  KFbxScene* scene;
+
+  FbxArray<KString*> takeNames;
+
+  if(argc <= 1)
+    *gFileName = "humanoid.fbx";
+  else
+    *gFileName = argv[1];
+
+  // The first thing to do is to create the FBX SDK manager which is the
+  // object allocator for almost all the classes in the SDK.
+  if (!(fbxSDKManager = KFbxSdkManager::Create()))
+    errx (EXIT_FAILURE, "Unable to create the FBX SDK manager");
+
+  // Load plugins from the executable directory
+  KString lPath = FbxGetApplicationDirectory();
+  KString lExtension = "so";
+
+  fbxSDKManager->LoadPluginsDirectory(lPath.Buffer(), lExtension.Buffer());
+
+  // Create the entity that will hold the scene.
+  scene = FbxScene::Create(fbxSDKManager, "");
+
+  if (!fbxSDKManager)
+    fprintf (stderr, "Unable to create the FBX SDK manager\n");
+
+  fbxImporter = KFbxImporter::Create(fbxSDKManager, "");
+
+  if (!fbxSDKManager->GetIOPluginRegistry()->DetectReaderFileFormat(*gFileName, lFileFormat))
+    lFileFormat = fbxSDKManager->GetIOPluginRegistry()->FindReaderIDByDescription ("FBX binary (*.fbx)");;
+
+  if(!fbxImporter->Initialize(gFileName->Buffer(), lFileFormat))
+    errx (EXIT_FAILURE, "Unable to open `%s': %s", (const char *) *gFileName, (const char *) fbxImporter->GetLastErrorString ());
+
+  if(!fbxImporter->Import(scene))
+    errx (EXIT_FAILURE, "Unable to import `%s': %s", (const char *) *gFileName, (const char *) fbxImporter->GetLastErrorString ());
+
+  // Convert Axis System to what is used in this example, if needed
+  KFbxAxisSystem OurAxisSystem (KFbxAxisSystem::eYAxis, FbxAxisSystem::eParityOdd, FbxAxisSystem::eRightHanded);
+
+  if (scene->GetGlobalSettings().GetAxisSystem() != OurAxisSystem)
+    OurAxisSystem.ConvertScene(scene);
+
+  if (scene->GetGlobalSettings().GetSystemUnit().GetScaleFactor() != 100.0)
+    {
+      FbxSystemUnit OurSystemUnit(100.0);
+      OurSystemUnit.ConvertScene(scene);
+    }
+
+  // Nurbs and patch attribute types are not supported yet.
+  // Convert them into mesh node attributes to have them drawn.
+  ConvertNurbsAndPatchRecursive(fbxSDKManager, scene->GetRootNode());
+
+  // Convert any .PC2 point cache data into the .MC format for
+  // vertex cache deformer playback.
+  PreparePointCacheData(scene);
+
+  scene->FillAnimStackNameArray (takeNames);
+
+  fbxImporter->Destroy();
+
+  ExportMeshScene (scene);
+
+  for (i = 0; i < takeNames.GetCount(); i++)
+    {
+      FbxTakeInfo* lCurrentTakeInfo;
+      KTime period, start, stop, currentTime;
+
+      FbxAnimStack *lCurrentAnimationStack;
+
+      lCurrentAnimationStack = scene->FindMember(FBX_TYPE(FbxAnimStack), takeNames[i]->Buffer());
+
+      if (lCurrentAnimationStack == NULL)
+      {
+        fprintf (stderr, "Animation stack %d not found!\n", i);
+
+        return EXIT_FAILURE;
+      }
+
+      scene->GetEvaluator()->SetContext(lCurrentAnimationStack);
+
+      lCurrentTakeInfo = scene->GetTakeInfo(*takeNames[i]);
+
+      if (!lCurrentTakeInfo)
+        continue;
+
+      printf ("begin-take %s\n", (const char *) *takeNames[i]);
+
+      period.SetMilliSeconds (10.0);
+
+      start = lCurrentTakeInfo->mLocalTimeSpan.GetStart();
+      stop = lCurrentTakeInfo->mLocalTimeSpan.GetStop();
+
+      for (currentTime = start; currentTime <= stop; currentTime += period)
+        ExportAnimScene (scene, currentTime);
+
+      printf ("end-take\n");
+    }
+
+  return 0;
+}
+
+static void
+ConvertNurbsAndPatchRecursive(KFbxSdkManager* pSdkManager, KFbxNode* node)
+{
+  KFbxNodeAttribute* lNodeAttribute;
+  int i, count;
+
+  lNodeAttribute = node->GetNodeAttribute();
+
+  if (lNodeAttribute
+      && (lNodeAttribute->GetAttributeType() == KFbxNodeAttribute::eNurbs
+          || lNodeAttribute->GetAttributeType() == KFbxNodeAttribute::ePatch))
+    {
+      FbxGeometryConverter lConverter(pSdkManager);
+
+      lConverter.TriangulateInPlace (node);
+    }
+
+  count = node->GetChildCount();
+
+  for (i = 0; i < count; ++i)
+    ConvertNurbsAndPatchRecursive(pSdkManager, node->GetChild(i));
+}
+
+static void
+PreparePointCacheData(FbxScene* pScene)
+{
+  // This function show how to cycle thru scene elements in a linear way.
+  int lIndex, lNodeCount;
+
+  lNodeCount = FbxGetSrcCount<FbxNode>(pScene);
+
+  for (lIndex = 0; lIndex < lNodeCount; lIndex++)
+    {
+      FbxNode* lNode;
+
+      lNode = FbxGetSrc<FbxNode>(pScene, lIndex);
+
+      if (lNode->GetGeometry())
+        {
+          int i, lVertexCacheDeformerCount;
+
+          lVertexCacheDeformerCount = lNode->GetGeometry()->GetDeformerCount(FbxDeformer::eVertexCache);
+
+          // There should be a maximum of 1 Vertex Cache Deformer for the moment
+          lVertexCacheDeformerCount = lVertexCacheDeformerCount > 0 ? 1 : 0;
+
+          for (i = 0; i < lVertexCacheDeformerCount; ++i)
+            {
+              KFbxVertexCacheDeformer* lDeformer;
+
+              lDeformer = static_cast<KFbxVertexCacheDeformer*>(lNode->GetGeometry()->GetDeformer(i, KFbxDeformer::eVertexCache));
+
+              if(!lDeformer)
+                continue;
+
+              KFbxCache* lCache = lDeformer->GetCache();
+
+              if(!lCache)
+                continue;
+
+              // Process the point cache data only if the constraint is active
+              if (!lDeformer->IsActive())
+                continue;
+
+              if (lCache->GetCacheFileFormat() == KFbxCache::eMayaCache)
+                {
+                  if (!lCache->ConvertFromMCToPC2(KTime::GetFrameRate(pScene->GetGlobalSettings().GetTimeMode()), 0))
+                    {
+                      // Conversion failed, retrieve the error here
+                      KString lTheErrorIs = lCache->GetError().GetLastErrorString();
+                    }
+                }
+
+              // Now open the cache file to read from it
+              if (!lCache->OpenFileForRead())
+                {
+                  // Cannot open file
+                  KString lTheErrorIs = lCache->GetError().GetLastErrorString();
+
+                  // Set the deformer inactive so we don't play it back
+                  lDeformer->SetActive(false);
+                }
+            }
+        }
+    }
+}
+
+// Get the geometry deformation local to a node. It is never inherited by the
+// children.
+KFbxXMatrix GetGeometry(KFbxNode* node)
+{
+  KFbxXMatrix lGeometry;
+
+  lGeometry.SetT(node->GetGeometricTranslation(KFbxNode::eSourcePivot));
+  lGeometry.SetR(node->GetGeometricRotation(KFbxNode::eSourcePivot));
+  lGeometry.SetS(node->GetGeometricScaling(KFbxNode::eSourcePivot));
+
+  return lGeometry;
+}
+
+struct VertexWeights
+{
+  double weights[4];
+  unsigned int bones[4];
+
+  VertexWeights()
+    {
+      memset (weights, 0, sizeof (weights));
+      memset (bones, 0, sizeof (bones));
+    }
+
+  void AddWeight (double weight, unsigned int bone)
+    {
+      unsigned int i;
+
+      for (i = 0; i < 4; ++i)
+        {
+          if (weights[i] < weight)
+            {
+              memmove (weights + i + 1, weights + i, (4 - i - 1) * sizeof (weights[0]));
+              memmove (bones + i + 1, bones + i, (4 - i - 1) * sizeof (bones[0]));
+
+              weights[i] = weight;
+              bones[i] = bone;
+
+              return;
+            }
+        }
+    }
+};
+
+static void
+ExportClusterDeformation(KFbxXMatrix& pGlobalPosition,
+                         KFbxMesh* pMesh)
+{
+    KFbxCluster::ELinkMode lClusterMode = ((KFbxSkin*)pMesh->GetDeformer(0, KFbxDeformer::eSkin))->GetCluster(0)->GetLinkMode();
+
+    int i, j;
+    int lClusterCount=0;
+    int lVertexCount = pMesh->GetControlPointsCount();
+    int lSkinCount=pMesh->GetDeformerCount(KFbxDeformer::eSkin);
+
+    VertexWeights *weights;
+
+    weights = new VertexWeights[lVertexCount];
+
+    for(i=0; i<lSkinCount; ++i)
+      {
+        lClusterCount = ((KFbxSkin *)pMesh->GetDeformer(i, KFbxDeformer::eSkin))->GetClusterCount();
+
+        for (j=0; j<lClusterCount; ++j)
+          {
+            KFbxCluster* lCluster = ((KFbxSkin *) pMesh->GetDeformer(i, KFbxDeformer::eSkin))->GetCluster(j);
+            int k;
+
+            if (!lCluster->GetLink())
+              continue;
+
+            KFbxXMatrix lReferenceGlobalInitPosition;
+            KFbxXMatrix lClusterGlobalInitPosition;
+            KFbxXMatrix lReferenceGeometry;
+            KFbxXMatrix lClusterGeometry;
+
+            KFbxXMatrix lClusterRelativeInitPosition;
+            KFbxXMatrix lVertexTransformMatrix;
+
+            if (lClusterMode == FbxCluster::eAdditive && lCluster->GetAssociateModel())
+              {
+                lCluster->GetTransformAssociateModelMatrix(lReferenceGlobalInitPosition);
+              }
+            else
+              {
+                lCluster->GetTransformMatrix(lReferenceGlobalInitPosition);
+
+                lReferenceGeometry = GetGeometry(pMesh->GetNode());
+                lReferenceGlobalInitPosition *= lReferenceGeometry;
+              }
+
+            lCluster->GetTransformLinkMatrix(lClusterGlobalInitPosition);
+
+            lClusterRelativeInitPosition = lClusterGlobalInitPosition.Inverse() * lReferenceGlobalInitPosition;
+
+            printf ("bone-matrix %d", j);
+
+            for (k = 0; k < 16; ++k)
+              printf (" %.6g", lClusterRelativeInitPosition.Get (k / 4, k % 4));
+
+            printf ("\n");
+
+            int lVertexIndexCount = lCluster->GetControlPointIndicesCount();
+
+            for (k = 0; k < lVertexIndexCount; ++k)
+              {
+                int lIndex = lCluster->GetControlPointIndices()[k];
+                double lWeight = lCluster->GetControlPointWeights()[k];
+
+                if (lWeight == 0.0)
+                  continue;
+
+                weights[lIndex].AddWeight (lWeight, j);
+              }
+          }
+      }
+
+    for (i = 0; i < lVertexCount; i++)
+      {
+        printf ("bone-weights %d %.6g %.6g %.6g %.6g %u %u %u %u\n",
+                i,
+                weights[i].weights[0], weights[i].weights[1], weights[i].weights[2], weights[i].weights[3],
+                weights[i].bones[0], weights[i].bones[1], weights[i].bones[2], weights[i].bones[3]);
+      }
+
+    delete [] weights;
+}
+
+static void
+ExportMesh(KFbxNode* node, KFbxXMatrix& pGlobalPosition)
+{
+  KFbxMesh* mesh;
+  int lClusterCount = 0;
+  int lSkinCount = 0;
+  int lVertexCount;
+  int i;
+
+  std::map<vertex, int> vertexMap;
+  std::vector<vertex> vertexArray;
+
+  mesh = node->GetMesh ();
+  lVertexCount = mesh->GetControlPointsCount();
+
+  if (!lVertexCount)
+    return;
+
+  lSkinCount = mesh->GetDeformerCount(KFbxDeformer::eSkin);
+
+  for (int i = 0; i < lSkinCount; i++)
+    lClusterCount += ((KFbxSkin *)(mesh->GetDeformer(i, KFbxDeformer::eSkin)))->GetClusterCount();
+
+  printf ("begin-mesh\n");
+
+    {
+      int lMaterialIndex;
+      int lTextureIndex;
+      FbxProperty lProperty;
+      int lNbTex;
+      FbxSurfaceMaterial *lMaterial = NULL;
+      int lNbMat = node->GetSrcObjectCount(FbxSurfaceMaterial::ClassId);
+
+      for (lMaterialIndex = 0; lMaterialIndex < lNbMat; lMaterialIndex++)
+        {
+          lMaterial = FbxCast <FbxSurfaceMaterial>(node->GetSrcObject(FbxSurfaceMaterial::ClassId, lMaterialIndex));
+
+          if(!lMaterial)
+            continue;
+
+          lProperty = lMaterial->FindProperty(FbxSurfaceMaterial::sDiffuse);
+
+          if(!lProperty.IsValid())
+            continue;
+
+          lNbTex = lProperty.GetSrcObjectCount(FbxTexture::ClassId);
+
+          for (lTextureIndex = 0; lTextureIndex < lNbTex; lTextureIndex++)
+            {
+              FbxFileTexture *lFileTexture;
+
+              if (!(lFileTexture = FbxCast<FbxFileTexture>(lProperty.GetSrcObject(FbxTexture::ClassId, lTextureIndex))))
+                continue;
+
+              printf ("texture %s\n", (const char *) lFileTexture->GetFileName ());
+            }
+        }
+    }
+
+  printf ("matrix");
+  for (i = 0; i < 16; ++i)
+    printf (" %.6g", pGlobalPosition.Get (i / 4, i % 4));
+  printf ("\n");
+
+  if (lClusterCount)
+    ExportClusterDeformation (pGlobalPosition, mesh);
+
+  FbxStringList lUVNames;
+  const char * lUVName;
+
+  mesh->GetUVSetNames(lUVNames);
+
+  if (lUVNames.GetCount())
+    lUVName = lUVNames[0];
+  else
+    lUVName = 0;
+
+  for (i = 0; i < mesh->GetPolygonCount(); i++)
+    {
+      int j, polygonSize;
+      int *indices;
+
+      polygonSize = mesh->GetPolygonSize(i);
+
+      indices = new int[polygonSize];
+
+      for (j = 0; j < polygonSize; j++)
+        {
+          vertex newVertex;
+
+          double *xyz;
+          FbxVector2 uv;
+
+          xyz = (double *) mesh->GetControlPoints()[mesh->GetPolygonVertex(i, j)];
+
+          newVertex.x = xyz[0];
+          newVertex.y = xyz[1];
+          newVertex.z = xyz[2];
+
+          mesh->GetPolygonVertexUV (i, j, lUVName, uv);
+
+          newVertex.u = uv[0];
+          newVertex.v = uv[1];
+
+          auto oldVertex = vertexMap.find (newVertex);
+
+          if (oldVertex != vertexMap.end ())
+            indices[j] = oldVertex->second;
+          else
+            {
+              indices[j] = vertexArray.size ();
+
+              vertexArray.push_back (newVertex);
+              vertexMap[newVertex] = indices[j];
+            }
+        }
+
+      for (j = 2; j < polygonSize; ++j)
+        printf ("triangle %d %d %d\n", indices[0], indices[j - 1], indices[j]);
+
+      delete [] indices;
+    }
+
+  for (i = 0; i < vertexArray.size (); ++i)
+    {
+      vertex v;
+
+      v = vertexArray[i];
+
+      printf ("xyz %d %.6g %.6g %.6g\n", i, v.x, v.y, v.z);
+      printf ("uv %d %.6g %.6g\n", i, v.u, v.v);
+    }
+
+  printf ("end-mesh\n");
+
+  fflush (stdout);
+}
+
+static void
+ExportMeshRecursive(KFbxNode* node,
+                    KFbxXMatrix& pParentGlobalPosition)
+{
+  KTime time;
+
+  // Compute the node's global position.
+  KFbxXMatrix lGlobalPosition = node->EvaluateGlobalTransform(time);
+
+  // Geometry offset.
+  // it is not inherited by the children.
+  KFbxXMatrix lGeometryOffset = GetGeometry(node);
+  KFbxXMatrix lGlobalOffPosition = lGlobalPosition * lGeometryOffset;
+
+  KFbxNodeAttribute* lNodeAttribute = node->GetNodeAttribute();
+
+  if (lNodeAttribute && lNodeAttribute->GetAttributeType() == KFbxNodeAttribute::eMesh)
+    ExportMesh(node, lGlobalPosition);
+
+  int i, lCount = node->GetChildCount();
+
+  for (i = 0; i < lCount; i++)
+    ExportMeshRecursive(node->GetChild(i), lGlobalPosition);
+}
+
+static void
+ExportMeshScene(KFbxScene* pScene)
+{
+  KFbxXMatrix lDummyGlobalPosition;
+
+  int i, lCount = pScene->GetRootNode()->GetChildCount();
+
+  for (i = 0; i < lCount; i++)
+    ExportMeshRecursive(pScene->GetRootNode()->GetChild(i), lDummyGlobalPosition);
+}
+
+static void ExportClusterDeformation(KFbxXMatrix& pGlobalPosition,
+                              KFbxMesh* pMesh,
+                              KTime& time)
+{
+  KFbxCluster::ELinkMode lClusterMode;
+  KFbxSkin *skin;
+
+  skin = (KFbxSkin *) pMesh->GetDeformer(0, KFbxDeformer::eSkin);
+
+  if (!skin)
+    return;
+
+  lClusterMode = skin->GetCluster(0)->GetLinkMode();
+
+  int i, j;
+  int lClusterCount=0;
+  int lVertexCount = pMesh->GetControlPointsCount();
+  int lSkinCount=pMesh->GetDeformerCount(KFbxDeformer::eSkin);
+
+  if (!lVertexCount)
+    return;
+
+  for(i=0; i<lSkinCount; ++i)
+    {
+      lClusterCount =( (KFbxSkin *)pMesh->GetDeformer(i, KFbxDeformer::eSkin))->GetClusterCount();
+
+      for (j=0; j<lClusterCount; ++j)
+        {
+          KFbxCluster* lCluster =((KFbxSkin *) pMesh->GetDeformer(i, KFbxDeformer::eSkin))->GetCluster(j);
+          int k;
+
+          if (!lCluster->GetLink())
+            continue;
+
+          KFbxXMatrix lReferenceGlobalCurrentPosition;
+          KFbxXMatrix lClusterGlobalInitPosition;
+          KFbxXMatrix lClusterGlobalCurrentPosition;
+          KFbxXMatrix lReferenceGeometry;
+          KFbxXMatrix lClusterGeometry;
+
+          KFbxXMatrix lClusterRelativeInitPosition;
+          KFbxXMatrix lClusterRelativeCurrentPositionInverse;
+          KFbxXMatrix lVertexTransformMatrix;
+
+          if (lClusterMode == FbxCluster::eAdditive && lCluster->GetAssociateModel())
+            {
+              lReferenceGlobalCurrentPosition = lCluster->GetAssociateModel()->EvaluateGlobalTransform(time);
+
+              // Geometric transform of the model
+              lReferenceGeometry = GetGeometry(lCluster->GetAssociateModel());
+              lReferenceGlobalCurrentPosition *= lReferenceGeometry;
+            }
+          else
+            lReferenceGlobalCurrentPosition = pGlobalPosition;
+
+
+          lClusterGlobalCurrentPosition = lCluster->GetLink()->EvaluateGlobalTransform(time);
+
+          // Compute the current position of the link relative to the reference.
+          lClusterRelativeCurrentPositionInverse = lReferenceGlobalCurrentPosition.Inverse() * lClusterGlobalCurrentPosition;
+
+#if 0
+          // Compute the shift of the link relative to the reference.
+          lVertexTransformMatrix = lClusterRelativeCurrentPositionInverse * lClusterRelativeInitPosition;
+#endif
+
+          printf ("bone-matrix %d", j);
+
+          for (k = 0; k < 16; ++k)
+            printf (" %.6g", lClusterRelativeCurrentPositionInverse.Get (k / 4, k % 4));
+
+          printf ("\n");
+        }
+    }
+}
+
+static void
+ExportAnimRecursive(KFbxNode* node,
+                    KTime& time,
+                    KFbxXMatrix& pParentGlobalPosition)
+{
+  KFbxXMatrix lGlobalPosition, lGeometryOffset, lGlobalOffPosition;
+  KFbxNodeAttribute* lNodeAttribute;
+  int i, count;
+
+  lGlobalPosition = node->EvaluateGlobalTransform(time);
+  lGeometryOffset = GetGeometry(node);
+  lGlobalOffPosition = lGlobalPosition * lGeometryOffset;
+
+  lNodeAttribute = node->GetNodeAttribute();
+
+  if (lNodeAttribute
+      && lNodeAttribute->GetAttributeType() == KFbxNodeAttribute::eMesh)
+    {
+      ExportClusterDeformation (lGlobalPosition, (KFbxMesh *) node->GetNodeAttribute(), time);
+    }
+
+  count = node->GetChildCount();
+
+  for (i = 0; i < count; ++i)
+    ExportAnimRecursive(node->GetChild(i), time, lGlobalPosition);
+}
+
+static void
+ExportAnimScene(KFbxScene* pScene, KTime& time)
+{
+  KFbxXMatrix lDummyGlobalPosition;
+
+  int i, lCount = pScene->GetRootNode()->GetChildCount();
+
+  printf ("begin-frame\n");
+  printf ("time %.3f\n", time.GetSecondDouble());
+
+  for (i = 0; i < lCount; i++)
+    ExportAnimRecursive(pScene->GetRootNode()->GetChild(i), time, lDummyGlobalPosition);
+
+  printf ("end-frame\n");
+}
---
+++ watch-subdirs.c
@@ -0,0 +1,196 @@
+#define _BSD_SOURCE
+
+#include <err.h>
+#include <fcntl.h>
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <strings.h>
+#include <sys/inotify.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <getopt.h>
+
+static int WatchSubdirs_printHelp;
+static int WatchSubdirs_printVersion;
+static const char *WatchSubdirs_makefile = "Makefile";
+
+static pthread_mutex_t build_mutex = PTHREAD_MUTEX_INITIALIZER;
+static pthread_cond_t build_cond = PTHREAD_COND_INITIALIZER;
+static uint64_t dirtyPaths; /* XXX: Supports max 64 paths */
+
+static char **paths;
+static int pathCount;
+
+extern char **environ;
+
+static void *
+WatchSubdirs_BuildThread (void *arg)
+{
+  for (;;)
+    {
+      pid_t child;
+      int status, pathIndex;
+
+      pthread_mutex_lock (&build_mutex);
+
+      while (!dirtyPaths)
+        pthread_cond_wait (&build_cond, &build_mutex);
+
+      pthread_mutex_unlock (&build_mutex);
+
+      /* Defer building for a little while, just in case we get multiple events in rapid succession */
+      usleep (100000);
+
+      while (0 != (pathIndex = ffs (dirtyPaths)))
+        {
+          --pathIndex;
+
+          if (-1 == (child = fork ()))
+            err (EXIT_FAILURE, "fork failed");
+
+          if (!child)
+            {
+              char *args[7];
+
+              args[0] = "/usr/bin/make";
+              args[1] = "-s";
+              args[2] = "-f";
+              args[3] = (char *) WatchSubdirs_makefile;
+              args[4] = "-C";
+              args[5] = paths[pathIndex];
+              args[6] = 0;
+
+              execve (args[0], args, environ);
+
+              exit (EXIT_FAILURE);
+            }
+
+          waitpid (child, &status, 0);
+
+          pthread_mutex_lock (&build_mutex);
+          dirtyPaths &= ~(1 << pathIndex);
+          pthread_mutex_unlock (&build_mutex);
+        }
+    }
+}
+
+static void
+WatchSubdirs_StartBuildThread (void)
+{
+  pthread_t threadHandle;
+
+  pthread_create (&threadHandle, 0, WatchSubdirs_BuildThread, 0);
+  pthread_detach (threadHandle);
+}
+
+static struct option long_options[] =
+{
+    { "makefile", required_argument, 0, 'm' },
+    { "help",     no_argument, &WatchSubdirs_printHelp, 1 },
+    { "version",  no_argument, &WatchSubdirs_printVersion, 1 },
+    { 0, 0, 0, 0 }
+};
+
+int
+main (int argc, char **argv)
+{
+  int i, inotifyFD;
+  char buffer[4096];
+  size_t bufferFill = 0;
+  ssize_t result;
+
+  int *watchDescriptors;
+
+  if (-1 == (inotifyFD = inotify_init1(IN_CLOEXEC)))
+    err (EXIT_FAILURE, "inotify_init1 failed");
+
+  while(-1 != (i = getopt_long(argc, argv, "", long_options, NULL)))
+    {
+      switch(i)
+        {
+        case 0:
+
+          break;
+
+        case 'm':
+
+          WatchSubdirs_makefile = optarg;
+
+          break;
+
+        case '?':
+
+          fprintf(stderr, "Try `%s --help' for more information.\n", argv[0]);
+
+          return EXIT_FAILURE;
+        }
+    }
+
+  if(WatchSubdirs_printHelp)
+    {
+      fprintf(stdout,
+              "Usage: %s [OPTION]... [DIRECTORY]...\n"
+              "\n"
+              "  -m, --makefile=<filename>  set path of Makefile\n"
+              "      --help     display this help and exit\n"
+              "      --version  display version information\n"
+              "\n"
+              "Report bugs to <morten.hustveit@gmail.com>\n", argv[0]);
+
+      return EXIT_SUCCESS;
+    }
+
+  paths = argv + optind;
+  pathCount = argc - optind;
+
+  if (!(watchDescriptors = calloc (pathCount, sizeof (*watchDescriptors))))
+    err (EXIT_FAILURE, "calloc failed");
+
+  for (i = 0; i < pathCount; ++i)
+    {
+      if(-1 == (watchDescriptors[i] = inotify_add_watch (inotifyFD, paths[i], IN_CLOSE_WRITE | IN_MOVED_TO | IN_ONLYDIR)))
+        err (EXIT_FAILURE, "inotify_add_watch: failed to start watching `%s'", paths[i]);
+    }
+
+  WatchSubdirs_StartBuildThread ();
+
+  for (;;)
+    {
+      if (-1 == (result = read (inotifyFD, buffer + bufferFill, sizeof (buffer) - bufferFill)))
+        err (EXIT_FAILURE, "read failed on inotify descriptor");
+
+      bufferFill += result;
+
+      while (bufferFill > sizeof (struct inotify_event))
+        {
+          struct inotify_event *event;
+          size_t eventSize;
+
+          event = (struct inotify_event *) buffer;
+          eventSize = sizeof(*event) + event->len;
+
+          if (event->name[0] != '.')
+            {
+              int pathIndex;
+
+              for (pathIndex = 0; pathIndex < pathCount; ++pathIndex)
+                {
+                  if (watchDescriptors[pathIndex] == event->wd)
+                    {
+                      pthread_mutex_lock (&build_mutex);
+                      dirtyPaths |= (1 << pathIndex);
+                      pthread_cond_signal (&build_cond);
+                      pthread_mutex_unlock (&build_mutex);
+
+                      break;
+                    }
+                }
+            }
+
+          bufferFill -= eventSize;
+          memmove (buffer, buffer + eventSize, bufferFill);
+        }
+    }
+}
